b44cda5c512e1779bc47caaa2bf6e93223c7f028
Krishna Singh <krishna22work.9@gmail.com>
2026-07-22 08:59:31 +0530

    2. Input a string of even length and return the second half of that string using inbuilt substr function

--- string/stringAll.java
+++ string/stringAll.java
@@ -9,14 +9,20 @@ public class stringAll {
         System.out.print("Enter the string : ");
         String s=sc.nextLine();
  int n=s.length();
-//      . Input a string of length greater than 5 and reverse the substring from position 2 to 5 using inbuilt
-//functions.
 
+//     1 . Input a string of length greater than 5 and reverse the substring from position 2 to 5 using inbuilt  functions.
         // chekeingthe length is greater than 5
         if(n>5){
             StringBuilder sb= new StringBuilder(s.substring(2,5)).reverse();
             System.out.println(sb);
         }else System.out.println(" length of the string id less than 5 ");
+
+//        2. Input a string of even length and return the second half of that string using inbuilt substr function
+        if (n%2==0){
+            StringBuilder ab= new StringBuilder(s.substring(n/2));
+            System.out.println(ab);
+        }else System.out.println("string length is not eveen ");
+
 
     }
 }
