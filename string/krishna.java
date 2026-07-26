ccd8f2cbba1f1309854b16c71be6885482bb6d5c
Krishna Singh <krishna22work.9@gmail.com>
2026-07-26 18:03:25 +0530

    Delete string/krishna.java

--- string/krishna.java
+++
@@ -1,7 +0,0 @@
-package string;
-
-public class krishna {
-    public static void main(String[] args) {
-        System.out.println(" this is the file   for just geting the strick  of of the ");
-    }
-}
